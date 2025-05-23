906f9341442d3b0200c831cd89ddaf94aa1220a9
ruihuang1124 <ruihuang.scut@gmail.com>
2025-05-23 18:18:42 +0800

    fix for build with humble

--- libraries/controller_common/src/FSM/StatePassive.cpp
+++ libraries/controller_common/src/FSM/StatePassive.cpp
@@ -13,23 +13,23 @@ void StatePassive::enter()
 {
     for (auto i : ctrl_interfaces_.joint_torque_command_interface_)
     {
-        std::ignore = i.get().set_value(0.0);
+        i.get().set_value(0.0);
     }
     for (auto i : ctrl_interfaces_.joint_position_command_interface_)
     {
-        std::ignore = i.get().set_value(0.0);
+        i.get().set_value(0.0);
     }
     for (auto i : ctrl_interfaces_.joint_velocity_command_interface_)
     {
-        std::ignore = i.get().set_value(0.0);
+        i.get().set_value(0.0);
     }
     for (auto i : ctrl_interfaces_.joint_kp_command_interface_)
     {
-        std::ignore = i.get().set_value(0.0);
+        i.get().set_value(0.0);
     }
     for (auto i : ctrl_interfaces_.joint_kd_command_interface_)
     {
-        std::ignore = i.get().set_value(1.0);
+        i.get().set_value(1.0);
     }
     ctrl_interfaces_.control_inputs_.command = 0;
 }
